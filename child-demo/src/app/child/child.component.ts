486647372b14d9122a34f7151e748b60a5e1fa26
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-02-20 12:45:26 +0600

    Getters and Setters

--- child-demo/src/app/child/child.component.ts
+++ child-demo/src/app/child/child.component.ts
@@ -6,9 +6,26 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
- @Input('loggedIn') loggedInFlag: boolean ;
+private _loggedIn: boolean ;
 
+message:string;
 
+get loggedIn():boolean{
+  return this._loggedIn;
+
+}
+@Input()
+
+set loggedIn(value:boolean){
+   this._loggedIn = value;
+  if(value===true){
+    this.message = "welcome  back  to  home";
+  }
+  else{
+    this.message = "please log  in first";
+    
+  }
+}
 
   constructor() { }
 
